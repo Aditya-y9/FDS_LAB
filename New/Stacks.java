c54404000cb0dbae046689d7a9e42f807c68820f
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-10-19 16:55:37 +0530

    Added Implementation Programs

--- New/Stacks.java
+++ New/Stacks.java
@@ -5,7 +5,7 @@ public class Stacks{
         Node next;
         public Node(int data){
             this.data=data;
-            this.next=null;
+           this.next=null;
         }
     }
     static class Stack{
