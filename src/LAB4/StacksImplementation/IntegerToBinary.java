8404b34d77b15e5fed4c1e3dd7e529ecf0888fad
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-10-27 01:48:02 +0530

    Done

--- src/LAB4/StacksImplementation/IntegerToBinary.java
+++ src/LAB4/StacksImplementation/IntegerToBinary.java
@@ -1,13 +1,5 @@
 package LAB4.StacksImplementation;
-
 import LAB4.*;
-
-/**
- * The IntegerToBinary class converts an integer to its binary representation.
- * It contains a main method that takes an integer and prints its binary representation.
- * The binary representation is obtained by dividing the integer by 2 and pushing the remainder onto a stack.
- * The binary representation is then obtained by popping the elements from the stack and printing them.
- */
 public class IntegerToBinary {
     public static void main(String[] args) {
         int num = 10;
