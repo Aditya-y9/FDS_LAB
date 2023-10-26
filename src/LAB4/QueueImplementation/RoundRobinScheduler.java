afcb59f8d363ee78d37ef25331b601b94a39a125
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-10-27 01:45:55 +0530

    Atleast

--- src/LAB4/QueueImplementation/RoundRobinScheduler.java
+++ src/LAB4/QueueImplementation/RoundRobinScheduler.java
@@ -2,36 +2,37 @@ package LAB4.QueueImplementation;
 import LAB4.*;
 
 public class RoundRobinScheduler {
-    public static void main(String[] args) {
-        // Create a new ArrayQueue with a capacity of 10
-        ArrayQueue<String> queue = new ArrayQueue<>(10);
+  public static void main(String[] args) {
+    // Create a new ArrayQueue with a capacity of 10
+    ArrayQueue<String> queue = new ArrayQueue<>(10);
 
-        // Add some processes to the queue
-        queue.enqueue("Process 1");
-        queue.enqueue("Process 2");
-        queue.enqueue("Process 3");
-        queue.enqueue("Process 4");
-        queue.enqueue("Process 5");
+    // Add some processes to the queue
+    queue.enqueue("Process 1");
+    queue.enqueue("Process 2");
+    queue.enqueue("Process 3");
+    queue.enqueue("Process 4");
+    queue.enqueue("Process 5");
 
-        // Set the time quantum for the round-robin scheduler
-        int timeQuantum = 2;
+    // Set the time quantum for the round-robin scheduler
+    int timeQuantum = 2;
 
-        // Run the round-robin scheduler
-        while (!queue.isEmpty()) {
-            // Get the next process from the queue
-            String process = queue.dequeue();
+    // Run the round-robin scheduler
+    while (!queue.isEmpty()) {
+      // Get the next process from the queue
+      String process = queue.dequeue();
 
-            // Run the process for the time quantum
-            System.out.println("Running process " + process + " for " + timeQuantum + " seconds...");
+      // Run the process for the time quantum
+      System.out.println("Running process " + process + " for " + timeQuantum +
+                         " seconds...");
 
-            // If the process is not finished, add it back to the queue
-            if (Math.random() < 0.5) {
-                System.out.println("Process " + process + " not finished, adding back to queue...");
-                queue.enqueue(process);
-            } else {
-                System.out.println("Process " + process + " finished.");
-            }
-        }
+      // If the process is not finished, add it back to the queue
+      if (Math.random() < 0.5) {
+        System.out.println("Process " + process +
+                           " not finished, adding back to queue...");
+        queue.enqueue(process);
+      } else {
+        System.out.println("Process " + process + " finished.");
+      }
     }
+  }
 }
-    
--- src/LAB4/StacksImplementation/DelimiterMatching.java
+++ src/LAB4/StacksImplementation/DelimiterMatching.java
@@ -1,46 +1,27 @@
 package LAB4.StacksImplementation;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 import LAB4.*;
 public class DelimiterMatching {
-    /**
-     * This method checks if the delimiters in the input string are matching or not.
-     *
-     * @param input the input string
-     * @return true if the delimiters are matching, false otherwise
-     */
     public static boolean isDelimiterMatching(String input) {
-        // Create a stack of characters.
         ArrayStack<Character> stack = new ArrayStack<Character>(input.length());
         for (int i = 0; i < input.length(); i++) {
             char currentChar = input.charAt(i);
-            // If the current character is an opening delimiter, then push it onto the stack.
             if (currentChar == '(' || currentChar == '[' || currentChar == '{') {
                 stack.push(currentChar);
             } else if (currentChar == ')' || currentChar == ']' || currentChar == '}') {
-                // If the current character is a closing delimiter, then pop the top character from the stack.
                 if (stack.isEmpty()) {
                     return false;
                 }
                 char topChar = stack.pop();
-                // If the popped character does not match the current character, then the delimiters are not matching.
                 if ((currentChar == ')' && topChar != '(') || (currentChar == ']' && topChar != '[')
                         || (currentChar == '}' && topChar != '{')) {
-                    // so return false.
                     return false;
                 }
             }
         }
-        // if the stack is empty, then all the opening delimiters have been matched with closing delimiters.
         return stack.isEmpty();
-    }
-
-    /**
-     * The main method to test the isDelimiterMatching method.
-     *
-     * @param args the command line arguments
-     */
+    }  
     public static void main(String[] args) {
         System.out.println("Enter the input string: ");
         Scanner s = new Scanner(System.in);
