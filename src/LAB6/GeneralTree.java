6bb8f9a78fa4b8d410fcc2cab948e74bfd863b96
Aditya-y9 <aditya.yedurkar@gmail.com>
2023-11-09 23:15:38 +0530

    Added Docs

--- src/LAB6/GeneralTree.java
+++ src/LAB6/GeneralTree.java
@@ -3,8 +3,22 @@ package LAB6;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A general tree data structure that stores elements of type E. Each node in the tree can have an arbitrary number of
+ * children. The tree is represented using a linked structure where each node stores a reference to its parent and a list
+ * of references to its children. The tree supports operations to add a root, add a child to a node, and attach a tree as
+ * a subtree to a node. It also supports a method to display the tree in a hierarchical format.
+ *
+ * @param <E> the type of elements stored in the tree
+ */
 public class GeneralTree<E> {
 
+    /**
+     * A node in the GeneralTree data structure that stores an element of type E, a reference to its parent node, and a list
+     * of references to its children nodes.
+     *
+     * @param <E> the type of element stored in the node
+     */
     protected static class Node<E> implements Position<E> {
         private E element;
         private Node<E> parent;
@@ -17,31 +31,60 @@ public class GeneralTree<E> {
         }
 
         @Override
+        /**
+         * @return the element stored in the node
+         */
         public E getElement() {
             return this.element;
         }
 
+        /**
+         * @return the parent node of the current node
+         */
         public Node<E> getParent() {
             return this.parent;
         }
 
+        /**
+         * @return the list of children nodes of the current node
+         */
         public List<Node<E>> getChildren() {
             return this.children;
         }
 
+        /**
+         * @return true if the current node is a leaf node, false otherwise
+         */
         public boolean isLeaf() {
             return this.children.size() == 0;
         }
 
+        /**
+         * Sets the parent node of the current node to the specified node.
+         *
+         * @param p the node to be set as the parent node of the current node
+         */
         public void setParent(Node<E> p) {
             this.parent = p;
         }
 
+        /**
+         * Adds the specified node as a child node of the current node.
+         *
+         * @param child the node to be added as a child node of the current node
+         */
         public void addChild(Node<E> child) {
             this.children.add(child);
         }
     }
 
+    /**
+     * Creates a new node with the specified element and parent node.
+     *
+     * @param e the element to be stored in the node
+     * @param parent the parent node of the node to be created
+     * @return the newly created node
+     */
     protected Node<E> createNode(E e, Node<E> parent) {
         return new Node<>(e, parent);
     }
@@ -49,11 +92,21 @@ public class GeneralTree<E> {
     private Node<E> root;
     private int size;
 
+    /**
+     * Constructs an empty GeneralTree data structure.
+     */
     GeneralTree() {
         this.root = null;
         this.size = 0;
     }
 
+    /**
+     * Returns the node corresponding to the specified position.
+     *
+     * @param p the position of the node to be returned
+     * @return the node corresponding to the specified position
+     * @throws IllegalArgumentException if the specified position is not a valid position in the GeneralTree data structure
+     */
     protected Node<E> validate(Position<E> p) throws IllegalArgumentException {
         if (!(p instanceof Node))
             throw new IllegalArgumentException("Not valid position type");
@@ -63,14 +116,31 @@ public class GeneralTree<E> {
         return posNode;
     }
 
+    /**
+     * Returns the number of nodes in the GeneralTree data structure.
+     *
+     * @return the number of nodes in the GeneralTree data structure
+     */
     public int size() {
         return this.size;
     }
 
+    /**
+     * Returns true if the GeneralTree data structure is empty, false otherwise.
+     *
+     * @return true if the GeneralTree data structure is empty, false otherwise
+     */
     public boolean isEmpty() {
         return this.size == 0;
     }
 
+    /**
+     * Adds a root node to the GeneralTree data structure with the specified element.
+     *
+     * @param e the element to be stored in the root node
+     * @return the position of the newly added root node
+     * @throws IllegalStateException if the GeneralTree data structure is not empty
+     */
     public Position<E> addRoot(E e) throws IllegalStateException {
         if (!this.isEmpty())
             throw new IllegalStateException("Tree is not empty");
@@ -79,6 +149,14 @@ public class GeneralTree<E> {
         return root;
     }
 
+    /**
+     * Adds a child node to the specified parent node in the GeneralTree data structure with the specified element.
+     *
+     * @param p the position of the parent node to which the child node is to be added
+     * @param e the element to be stored in the child node
+     * @return the position of the newly added child node
+     * @throws IllegalArgumentException if the specified position is not a valid position in the GeneralTree data structure
+     */
     public Position<E> addChild(Position<E> p, E e) throws IllegalArgumentException {
         Node<E> node = validate(p);
         Node<E> child = createNode(e, node);
@@ -87,6 +165,13 @@ public class GeneralTree<E> {
         return child;
     }
 
+    /**
+     * Attaches the specified GeneralTree data structure as a subtree to the specified parent node in the current GeneralTree data structure.
+     *
+     * @param p the position of the parent node to which the specified GeneralTree data structure is to be attached
+     * @param t the GeneralTree data structure to be attached as a subtree
+     * @throws IllegalArgumentException if the specified position is not a valid position in the GeneralTree data structure
+     */
     public void attach(Position<E> p, GeneralTree<E> t) throws IllegalArgumentException {
         Node<E> node = validate(p);
         this.size += t.size() - 1;
@@ -98,9 +183,14 @@ public class GeneralTree<E> {
         }
     }
 
-    // p is the position whose subtree is to be displayed
-    // level is for maintaining level of indentations b/w recursive calls
-    // label is for maintaining the prefix label b/w recursive calls - forEach loop is not used to use index as label
+    /**
+     * Displays the GeneralTree data structure in a hierarchical format starting from the specified position.
+     *
+     * @param p the position of the node from which the tree is to be displayed
+     * @param level the level of indentation for the current node
+     * @param label the prefix label for the current node
+     * @throws IllegalArgumentException if the specified position is not a valid position in the GeneralTree data structure
+     */
     public void display(Position<E> p, int level, String label) throws IllegalArgumentException {
         Node<E> node = validate(p);
         for (int i = 0; i < level; ++i)
